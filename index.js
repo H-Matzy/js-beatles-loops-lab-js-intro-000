405fa9f48e2d127d9b0192c723c85e951c27b4ac
Hayden Matz <haydenmatz97@gmail.com>
2020-10-02 19:26:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // add solution here
-function the BeatlesPlay(mus,ins){
+function theBeatlesPlay(mus,ins){
   var empty = []
   for (let i = 0, 1 = mus.lenght; i < 1; i++) {
     empty.push(`${mus[i]} plays ${ins[i]}`)
