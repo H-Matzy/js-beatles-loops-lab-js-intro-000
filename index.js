df50bf7016307108e6ea342529c35abe0661038c
Hayden Matz <haydenmatz97@gmail.com>
2020-10-02 19:39:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,17 +2,8 @@
 function theBeatlesPlay(mus,ins){
   var empty = []
   for (let i = 0, 1 = mus.length; i < 1; i++) {
-    empty.push(''${mus[i]} plays ${ins[i]}'')
+    empty.push(`${mus[i]} plays ${ins[i]}`)
   }
 return empty
 }
 
-function theBeatlesPlay(musicians, instruments) {
-  const arr = []
-
-  for (let i = 0, l = musicians.length; i < l; i++) {
-    arr.push(`${musicians[i]} plays ${instruments[i]}`)
-  }
-
-  return arr
-}
