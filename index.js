f25d529b48fc9722b034f68ceda7a31ccc284e4e
Hayden Matz <haydenmatz97@gmail.com>
2020-10-02 19:38:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+// add solution here
+function theBeatlesPlay(mus,ins){
+  var empty = []
+  for (let i = 0, 1 = mus.lenght; i < 1; i++) {
+    empty.push(''${mus[i]} plays ${ins[i]}'')
+  }
+return empty
+}
+
 function theBeatlesPlay(musicians, instruments) {
   const arr = []
 
