e7755e13cc7d870971841b3208988e71a9dd7e3f
Hayden Matz <haydenmatz97@gmail.com>
2020-10-02 19:41:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 // add solution here
-function theBeatlesPlay(mus,ins){
+function theBeatlesPlay(musicians,instruments){
   var empty = []
-  for (let i = 0, 1 = mus.length; i < 1; i++) {
-    empty.push(`${mus[i]} plays ${ins[i]}`)
+  for (let i = 0, 1 = musicians.length; i < 1; i++) {
+    empty.push(`${musicians[i]} plays ${instruments[i]}`)
   }
 return empty
+}
+function theBeatlesPlay(musicians, instruments) {
+  const arr = []
+
+  for (let i = 0, l = musicians.length; i < l; i++) {
+    arr.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+
+  return arr
 }
